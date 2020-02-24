7f0f63d707ed01ad8ad896f585abaddc00548b7a
hailita1 <hailit2306@gmail.com>
2020-02-24 09:28:49 +0700

    first commit

---
+++ src/main/java/com/codegym/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.codegym.repository;
+
+import com.codegym.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    Iterable<Customer> findAllByFirstNameContains(String firstName);
+}
